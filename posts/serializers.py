4e5352d8e650196f71e3b4d26b5ed55211da6759
josMuiruri <josemuiruri51@gmail.com>
2024-08-15 22:03:39 +0300

    authentication

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,8 +1,12 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
+
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
+
     class Meta:
+        model = Post
         fields = (
             'id',
             'author',
@@ -10,4 +14,9 @@ class PostSerializer(serializers.ModelSerializer):
             'body',
             'created_at',
         )
-        model = Post
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username',)
