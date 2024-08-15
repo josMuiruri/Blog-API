654a7627269ce1a17e4e23a2b5c2f47179c72670
josMuiruri <josemuiruri51@gmail.com>
2024-08-15 22:54:15 +0300

    schema and documentation

--- django_project/settings.py
+++ django_project/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'dj_rest_auth',
     'dj_rest_auth.registration',
+    'drf_spectacular',
     
     # local
     'accounts.apps.AccountsConfig',
@@ -155,6 +156,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 CORS_ORIGIN_WHITELIST = (
@@ -163,3 +165,9 @@ CORS_ORIGIN_WHITELIST = (
 )
 
 CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Blog API PROJECT',
+    'DESCRIPTION': 'Simple blog',
+    'VERSION': '1.0.0'
+}
